049caf1155ee1169a3a0b3d9f69c06255e40ce57
NguyenToan3107 <nguyentoan31072k3@gmail.com>
2024-03-03 22:11:50 +0700

    Completed Update/ delete user

--- app/src/main/java/net/branium/view/fragments/main/UserUpdateActivity.java
+++ app/src/main/java/net/branium/view/fragments/main/UserUpdateActivity.java
@@ -1,17 +1,15 @@
 package net.branium.view.fragments.main;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,23 +21,25 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import net.branium.R;
-import net.branium.view.fragments.main.UserFragment;
 
-public class UserUpdateFragment extends Fragment {
+public class UserUpdateActivity extends AppCompatActivity {
     String userUID;
     MaterialButton mt_btn_update_user_account;
+    MaterialButton mt_btn_back_user_account;
     EditText et_username_update;
     FirebaseAuth mAuth;
     FirebaseFirestore db;
     DocumentReference userRef;
     FirebaseUser firebaseUser;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_user_update, container, false);
-        mt_btn_update_user_account = view.findViewById(R.id.mt_btn_update_user_account);
-        et_username_update = view.findViewById(R.id.et_username_update);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_update);
+
+        mt_btn_update_user_account = findViewById(R.id.mt_btn_update_user_account);
+        mt_btn_back_user_account = findViewById(R.id.mt_btn_back_user_account);
+        et_username_update = findViewById(R.id.et_username_update);
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
@@ -47,18 +47,19 @@ public class UserUpdateFragment extends Fragment {
         userUID = mAuth.getCurrentUser().getUid();
         userRef = db.collection("users").document(userUID);
 
-        return view;
-    }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        GetUserByUID();
+        GetUserByUID(); // lấy usernaem, email từ firebase
 
         mt_btn_update_user_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 UpdateAccountUser();
+            }
+        });
+        mt_btn_back_user_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new UserFragment());
             }
         });
     }
@@ -67,17 +68,15 @@ public class UserUpdateFragment extends Fragment {
         String username = et_username_update.getText().toString();
         // update username
         userRef.update("username", username);
-
-        loadFragment(new UserFragment());
+        Toast.makeText(getApplicationContext(), "Cập nhật thành công", Toast.LENGTH_SHORT).show();
     }
 
     private void loadFragment(Fragment fragment) {
-        FragmentManager manager = getActivity().getSupportFragmentManager();
+        FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.body_container, fragment);
         transaction.commit();
     }
-
 
     private void GetUserByUID() {
         db.collection("users").document(userUID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -90,7 +89,7 @@ public class UserUpdateFragment extends Fragment {
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getContext(), "Lỗi: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Lỗi: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
